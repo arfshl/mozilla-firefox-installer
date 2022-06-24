eb2fcc293d96242a1813557dd69e2265767f46cb
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-24 17:48:36 +0700

    Update install-su.sh

--- install-su.sh
+++ install-su.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 echo "Mozilla Firefox Installler"
-echo "mv firefox-*.tar.bz2 /opt
-cd /opt
-tar xvjf firefox-*.tar.bz2
-rm firefox-*.tar.bz2
-ln -s /opt/firefox*/firefox /usr/local/bin/firefox
-wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications"
-read -p "Press any key to install..."
 mv firefox-*.tar.bz2 /opt
 cd /opt
 tar xvjf firefox-*.tar.bz2
