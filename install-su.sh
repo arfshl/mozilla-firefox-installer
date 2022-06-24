d4f357ff641c0fb5b973f1297a3e0988dbf3cb1b
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-24 13:39:14 +0700

    Update install-su.sh

--- install-su.sh
+++ install-su.sh
@@ -5,11 +5,11 @@ cd /opt
 tar xvjf firefox-*.tar.bz2
 rm firefox-*.tar.bz2
 ln -s /opt/firefox*/firefox /usr/local/bin/firefox
-wget https://raw.githubusercontent.com/arfshl/mozilla-firefox-installer/script/firefox.desktop -P /usr/local/share/applications"
-read -p "Press any key to execute script"
+wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications"
+read -p "Press any key to install"
 mv firefox-*.tar.bz2 /opt
 cd /opt
 tar xvjf firefox-*.tar.bz2
 rm firefox-*.tar.bz2
 ln -s /opt/firefox*/firefox /usr/local/bin/firefox
-wget https://raw.githubusercontent.com/arfshl/mozilla-firefox-installer/script/firefox.desktop -P /usr/local/share/applications
+wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
