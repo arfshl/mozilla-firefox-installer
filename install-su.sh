e77ff8dc5410433409ccf50026f2fa2d21781d1d
arfshl <98870352+arfshl@users.noreply.github.com>
2022-07-14 18:42:29 +0700

    Delete install-su.sh

--- install-su.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Mozilla Firefox Installler"
-wget https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US -P /opt
-cd /opt
-tar xvjf firefox-*.tar.bz2
-rm firefox-*.tar.bz2
-ln -s /opt/firefox*/firefox /usr/local/bin/firefox
-wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
