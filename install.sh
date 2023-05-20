8ef92da51346d5a9c0adea67183edea1ccaad5ab
arfshl <98870352+arfshl@users.noreply.github.com>
2023-05-20 01:34:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Installing Firefox..."
-wget https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US -P ~/
+wget https://ftp.mozilla.org/pub/firefox/releases/113.0.1/linux-x86_64/en-US/firefox-113.0.1.tar.bz2 -P ~/
 cd ~/
-sudo tar xvf firefox.*.tar
+sudo tar xjvf firefox.*.tar.bz2
 chmod -R 755 firefox
 ln -s ~/firefox/firefox /usr/local/bin/firefox
 wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
