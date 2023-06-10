32b5b2e611662feda72c8b66bccdd8def7aba671
arfshl <98870352+arfshl@users.noreply.github.com>
2023-06-10 02:05:55 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Installing Mozilla Firefox..."
-sudo wget https://ftp.mozilla.org/pub/firefox/releases/113.0.2/linux-x86_64/en-US/firefox-113.0.2.tar.bz2 -P /opt
+sudo wget https://ftp.mozilla.org/pub/firefox/releases/114.0.1/linux-x86_64/en-US/firefox-114.0.1.tar.bz2 -P /opt
 sudo tar xjvf /opt/firefox*.tar.bz2
 sudo chmod -R 755 /opt/firefox*
 ln -s /opt/firefox/firefox /usr/local/bin/firefox
