b79ed354a478b7cc0f9e2fe6a316b01b87b51232
arfshl <98870352+arfshl@users.noreply.github.com>
2025-03-15 21:15:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 echo "Installing Mozilla Firefox..."
 sudo wget https://ftp.mozilla.org/pub/firefox/releases/136.0.1/linux-x86_64/en-US/firefox-136.0.1.tar.xz -P /opt
 cd /opt
-sudo tar xjvf /opt/firefox*.tar.*
+sudo tar xvf /opt/firefox*.tar.*
 sudo chmod -R 755 /opt/firefox*
 sudo ln -s /opt/firefox/firefox /usr/local/bin/firefox
 sudo wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P /usr/local/share/applications
