977014ed587787772bd72a4dcb9e7291559bd0c6
arfshl <98870352+arfshl@users.noreply.github.com>
2025-03-23 21:38:07 +0700

    fix icons

--- install-local.sh
+++ install-local.sh
@@ -6,7 +6,7 @@ tar xvf ~/firefox*.tar.*
 ln -s ~/firefox/firefox ~/.local/bin/firefox
 wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P ~/.local/share/applications
 sed -i.bak -e "s|Exec=firefox %u|Exec=$HOME/firefox/firefox %u|" $HOME/.local/share/applications/firefox.desktop
-sed -i.bak -e "s|Icon=/opt/firefox/chrome/icons/default/default128.png|Icon=$HOME/firefox/chrome/icons/default/default128.png|" $HOME/.local/share/applications/firefox.desktop
+sed -i.bak -e "s|Icon=/opt/firefox/browser/chrome/icons/default/default128.png|Icon=$HOME/firefox/browser/chrome/icons/default/default128.png|" $HOME/.local/share/applications/firefox.desktop
 rm ~/firefox*.tar.*
 rm ~/install-local.sh
 echo "Done."
