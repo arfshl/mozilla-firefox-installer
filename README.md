0847107988304cf7625812117957d28f20d6ad2f
arfshl <98870352+arfshl@users.noreply.github.com>
2025-03-23 21:34:40 +0700

    Add Local Install

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # mozilla-firefox-installer
 Mozilla Firefox for Linux (Latest,non-ESR) from mozilla tarball script
-## Install Mozilla Firefox
+## Install Mozilla Firefox (All User)
 
     wget https://raw.githubusercontent.com/arfshl/mozilla-firefox-installer/main/install.sh -P ~/ && sudo sh ~/install.sh
+
+## Local User Only
+
+    wget https://raw.githubusercontent.com/arfshl/mozilla-firefox-installer/main/install.sh -P ~/ && sudo sh ~/install.sh
+
+## [Fix Apparmor](https://github.com/arfshl/mozilla-firefox-installer/blob/main/fix-apparmor.md)
  
 ## Alternative methods
 - Flatpak
---
+++ install-local.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Installing Mozilla Firefox..."
+wget https://ftp.mozilla.org/pub/firefox/releases/136.0.2/linux-x86_64/en-US/firefox-136.0.2.tar.xz -P $HOME
+cd $HOME
+tar xvf ~/firefox*.tar.*
+ln -s ~/firefox/firefox ~/.local/bin/firefox
+wget https://raw.githubusercontent.com/mozilla/sumo-kb/main/install-firefox-linux/firefox.desktop -P ~/.local/share/applications
+sed -i.bak -e "s|Exec=firefox %u|Exec=$HOME/firefox/firefox %u|" $HOME/.local/share/applications/firefox.desktop
+sed -i.bak -e "s|Icon=/opt/firefox/chrome/icons/default/default128.png|Icon=$HOME/firefox/chrome/icons/default/default128.png|" $HOME/.local/share/applications/firefox.desktop
+rm ~/firefox*.tar.*
+rm ~/install-local.sh
+echo "Done."
