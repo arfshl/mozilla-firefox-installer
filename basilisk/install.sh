335811d272e44b17de99ee23459075b8685b6ff2
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2025-04-16 14:25:16 +0700

    Create install.sh

---
+++ basilisk/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Installing Basilisk Web Browser..."
+sudo wget https://dl.basilisk-browser.org/basilisk-20250220145130.linux-x86_64-gtk3.tar.xz -P /opt
+cd /opt
+sudo tar xvf /opt/basilisk*.tar.*
+sudo chmod -R 755 /opt/basilisk*
+sudo ln -s /opt/basilisk/basilisk /usr/local/bin/basilisk
+sudo wget https://github.com/arfshl/basilisk-installer-linux/raw/main/basilisk.desktop -P /usr/local/share/applications
+sudo rm /opt/basilisk*.tar.*
+rm ~/install.sh
+basilisk
