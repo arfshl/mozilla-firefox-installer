b891b046ed6a3998914f3cb8dfebdbb6a3ab0ad7
Alif Putra Winata <98870352+arfshl@users.noreply.github.com>
2025-05-01 21:22:11 +0700

    Update install.sh

--- mozilla-thunderbird/install.sh
+++ mozilla-thunderbird/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Installing Thunderbird..."
-sudo wget https://ftp.mozilla.org/pub/thunderbird/releases/137.0.2/linux-x86_64/en-US/thunderbird-137.0.2.tar.xz -P /opt
+sudo wget https://ftp.mozilla.org/pub/thunderbird/releases/138.0/linux-x86_64/en-US/thunderbird-138.0.tar.xz -P /opt
 cd /opt
 sudo tar xvf /opt/thunderbird*.tar.*
 sudo chmod -R 755 /opt/thunderbird*
